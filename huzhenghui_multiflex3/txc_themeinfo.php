a608f1f84a9303fc722c094e151c5029c9586951
mambax7 <mambax7@gmail.com>
2015-06-06 07:24:08 -0400

    Original files

---
+++ huzhenghui_multiflex3/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "huzhenghui_multiflex3";
+	$txc_themeinfo["author"] = "ºúÕù»Ô";
+	$txc_themeinfo["author_url"] = "http://xoops.org.cn/userinfo.php?uid=8616";
+	$txc_themeinfo["detail_url"] = "http://1-2-3-4.info/webtemplates/multiflex3/";
+	$txc_themeinfo["download_url"] = "http://xoops.org.cn/modules/newbb/viewtopic.php?topic_id=11559";
+?>
